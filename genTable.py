f699a603ff4a791cbbb5677834d7979990017b64
e131135 <willia.r1.conklin@lmco.com>
2020-03-24 14:02:41 -0400

    Improved genTable.py.

--- genTable.py
+++ genTable.py
@@ -3,7 +3,9 @@
 import os
 import datetime
 
-tableStr='TABLE'
+# Set these variables for your particular environment/preferences
+tableStr='WORKBENCH'
+pathToWorkbenches="{}/work/testbays".format(os.environ['HOME'])
 
 
 # rm existing TABLE link, if any
@@ -15,7 +17,10 @@ if os.path.islink(tableStr):
 now = datetime.datetime.now()
 
 # Create a directory based on today's date
-dirStr = "table_{}.{}.{}".format(now.month, now.day, now.year-2000)
+dirStr = "{}/table_{}.{}.{}".format(pathToWorkbenches,
+                                    now.month,
+                                    now.day,
+                                    now.year-2000)
 if not os.path.isdir(dirStr):
   os.mkdir(dirStr)
   print("Created directory {}.".format(dirStr))
